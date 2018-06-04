29402b6fef042137985fb4e7cf5eb6944167c86c
Tom Kraak <thomas.kraak@teamaol.com>
2018-06-04 16:28:08 -0400

    test remainder of calculate

--- test/calculate.test.js
+++ test/calculate.test.js
@@ -4,3 +4,19 @@ import { calculate } from '../src/components/calculate';
 test('it adds 2 numbers', t => {
   t.is(calculate(4, 'add', 4), 8);
 });
+
+test('it subtracts 2 numbers', t => {
+  t.is(calculate(4, 'subtract', 4), 0);
+});
+
+test('it subtracts 2 numbers', t => {
+  t.is(calculate(4, 'subtract', 4), 0);
+});
+
+test('it multiplies 2 numbers', t => {
+  t.is(calculate(4, 'multiply', 4), 16);
+});
+
+test('it divides 2 numbers', t => {
+  t.is(calculate(4, 'divide', 4), 1);
+});
